1f31f278a53774cfdd38ae17739db40a741b39be
Rensjuh <42119934+Rensjuh@users.noreply.github.com>
2020-06-24 13:39:22 +0200

    Update bongobot.py

--- bongobot.py
+++ bongobot.py
@@ -56,8 +56,10 @@ async def on_message(message):
         await message.channel.send(f'Pong! {round(client.latency * 1000)}ms')
     if message.content.startswith('!8ball'):
         randomlist = ["Zeker", "Concentreer je en vraag het opnieuw", "Zonder twijfel", "Erg twijfelachtig", "Vertrouw er niet op", "Het is zeker", "Zoals ik het zie ja", "Mijn bronnen zeggen nee", "Kan ik niet voorspellen", "Tekenen wijzen op ja"]
-    await message.channel.send(random.choice(randomlist))
-    await ctx.message.add_reaction("🎱")
+        await message.channel.send(random.choice(randomlist))
+    if message.content.startswith('!rustchance'):
+        randomlist = ["<:blauw:721459552746340475>", "<:rood:721459537172758558>"]
+        await message.channel.send(random.choice(randomlist))
     
        
  
